39a54292753e01d609ff1c4606b467e999fef19a
허윤석 <81311278+hys021@users.noreply.github.com>
2024-07-28 17:48:21 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s="010";
+    int a,f,r;
+    char c;
+    cin >> a >> c >> f >> c >> r;
+    cout << s << c << r << c << f;
+    return 0;
+}
