d426e210a4dcf43fd528e27d6e94b03e9b9426cd
허윤석 <81311278+hys021@users.noreply.github.com>
2024-07-30 18:59:54 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin >> a >> b;
+    int m;
+    m = (a>b) ? a : b;
+    cout << m;
+    return 0;
+}
