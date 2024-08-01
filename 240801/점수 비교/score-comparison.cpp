eb64637e0bec17f8e4b178a52538f36eac293d6e
허윤석 <81311278+hys021@users.noreply.github.com>
2024-08-01 23:28:59 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/점수 비교/score-comparison.cpp
+++ 240801/점수 비교/score-comparison.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a1,b1,a2,b2;
-    cin >> a1 >> b1 >> a2 >> b2;
+    cin >> a1 >> a2 >> b1 >> b2;
     cout << (a1>b1 && a2>b2) << endl;
     return 0;
 }
