2fdd140bf63b8766de5c7927dd64458182302f53
허윤석 <81311278+hys021@users.noreply.github.com>
2024-08-06 21:57:20 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/문자 출력하기/print-text.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    int i=1;
+    while(i<=8){
+        cout << c;
+        i++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
