7e3a8d66f1be19b06d7542601105f01873280d92
허윤석 <81311278+hys021@users.noreply.github.com>
2024-08-12 20:36:23 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/높은 수에서 낮은 수까지/from-high-to-low.cpp
+++ 240812/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -5,8 +5,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     cin >> a >> b;
-    for(int i=b;i>=a;i--){
-        cout << i << " ";
+    if(a>b){
+        for(int i=a;i>=b;i--){
+            cout << i << " ";
+        }
+    }
+    else{
+        for(int i=b;i>=a;i--){
+            cout << i << " ";
+        }
     }
     return 0;
 }
