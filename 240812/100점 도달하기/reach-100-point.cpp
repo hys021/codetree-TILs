b021a818c40a18ee4b4583ceed766c30610ccdd1
허윤석 <81311278+hys021@users.noreply.github.com>
2024-08-12 21:08:42 +0900

    Title: 100점 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for(int i=n;i<=100;i++){
+        if(i>=90){
+            cout << "A" << " ";
+        }
+        else if(i>=80 && i<90){
+            cout << "B" << " ";
+        }
+        else if(i>=70 && i<80){
+            cout << "C" << " ";
+        }
+        else if(i>=60 && i<70){
+            cout << "D" << " ";
+        }
+        else{
+            cout << "F" << " ";
+        }
+
+    }
+    return 0;
+}
