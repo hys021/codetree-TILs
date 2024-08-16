79282952430b8651fe2213d6b56f498e8ec3fabc
허윤석 <81311278+hys021@users.noreply.github.com>
2024-08-16 22:40:30 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/3n + 1 수열/3n-plus-1-sequence.cpp
+++ 240816/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -6,6 +6,9 @@ int main() {
     int n,cnt=0;
     cin >> n;
     while(true){
+        if(n==1){
+            break;
+        }
         if(n%2==0){
             n/=2;
             cnt++;
@@ -13,9 +16,6 @@ int main() {
         else if(n%2!=0){
             n = n*3+1;
             cnt++;
-        }
-        if(n==1){
-            break;
         }
     }
     cout << cnt;
