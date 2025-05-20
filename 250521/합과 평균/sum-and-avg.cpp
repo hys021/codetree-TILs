8fe6a21ee81a8c89d04b880db74bb20600dc89cf
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-21 01:14:51 +0900

    Title: 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250521/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin >> A >> B;
+    cout << fixed;
+    cout.precision(1);
+    cout << A+B << " " << (float)(A+B)/2;
+    // Please write your code here.
+    return 0;
+}
