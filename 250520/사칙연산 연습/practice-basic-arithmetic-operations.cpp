20652d26a45030bf4e91c635ef6330ab7279a59c
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-20 16:39:55 +0900

    Title: 사칙연산 연습, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250520/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    a=5;
+    b=30;
+    c= a+b;
+    cout << c/8 << "\n" << c%8;
+    // Please write your code here.
+    return 0;
+}
