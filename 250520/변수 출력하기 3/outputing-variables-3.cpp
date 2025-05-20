c37e0896d1a9ae9b6ced33fbb1b0fc3a8623065d
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-20 16:41:12 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250520/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    char c;
+    a=1, b=2;
+    c='C';
+    cout << a << "->" << b << "->" << c;
+    // Please write your code here.
+    return 0;
+}
