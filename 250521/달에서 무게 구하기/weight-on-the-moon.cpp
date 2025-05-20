9b69050032975595675ae7e9aaca5cc4eac8ff1c
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-21 00:58:09 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250521/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int pound=13;
+    float rate=0.165;
+    
+    cout << fixed;
+    cout.precision(6);
+    cout << pound << " * " << rate << " = " << pound * rate;
+    // Please write your code here.
+    return 0;
+}
