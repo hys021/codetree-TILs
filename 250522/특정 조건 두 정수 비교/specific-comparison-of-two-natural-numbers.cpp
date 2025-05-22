7e042d75adb57d551fd1d3a52467cb2dfb72c67a
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-22 13:42:42 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250522/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    cin >> a >> b;
+    c = (a < b) ? 1 : 0;
+    d = (a == b) ? 1 : 0;
+    cout << c << " " << d;
+
+    // Please write your code here.
+    return 0;
+}
