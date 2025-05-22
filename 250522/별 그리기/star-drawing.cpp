73505ff892abcf9950963f18a5931da3ca326948
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-22 14:06:38 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250522/별 그리기/star-drawing.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int k=n-1; k>i; k--){
+            cout << " ";
+        }
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int k=0; k<=i; k++){
+            cout << " ";
+        }
+        for(int j=0; j<3-2*i; j++){
+            cout << "*";  
+        }
+        cout << endl;
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
