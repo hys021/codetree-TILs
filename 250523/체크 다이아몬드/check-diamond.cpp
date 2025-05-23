9140cc22a2256a751b567980c8ce3d3de7580840
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-23 22:34:23 +0900

    Title: 체크 다이아몬드, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250523/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=i;j<n-1;j++){
+            cout << " ";
+        }
+        for(int k=i;k>=0;k--){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i; j>=0; j--){
+            cout << " ";
+        }
+        for(int k=i;k<n-1;k++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    // Please write your code here.
+    return 0;
+}
