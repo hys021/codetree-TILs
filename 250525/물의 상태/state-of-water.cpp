5435eff13d78acdf78006368eb9b76ace5106428
허윤석 <81311278+hys021@users.noreply.github.com>
2025-05-25 23:47:07 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250525/물의 상태/state-of-water.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    if(n<0) cout << "ice";
+    else if(n>=100) cout << "vapor";
+    else cout << "water";
+    // Please write your code here.
+    return 0;
+}
